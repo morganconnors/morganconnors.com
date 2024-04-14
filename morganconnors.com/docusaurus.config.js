60c63b87f90fb8c15974f68f70e93ff212a0fb3e
morganconnors <github@morganconnors.com>
2024-04-13 22:07:34 -0400

    update

--- morganconnors.com/docusaurus.config.js
+++ morganconnors.com/docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         title: 'Home',
         logo: {
           alt: 'Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {to: '/docs/intro/', label: 'Docs', position: 'left'},
