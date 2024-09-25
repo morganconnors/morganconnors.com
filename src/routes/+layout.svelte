51bf80a01a3fe81f6ab912afd4235cdeb2e4ebd2
Morgan Connors <github@morganconnors.com>
2024-09-25 18:47:43 -0400

    Updated routes & file paths.
    Working on setting up +layout.svelte for a navigation bar that works &
    looks how I want.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/blog">Blog</a>
+		</li>
+		<li>
+			<a href="/resources">Resources</a>
+		</li>
+
+	</ul>
+</nav>
+
+<style>
+
+	nav {
+		display: flex;
+	}
+
+	ul {
+		display: flex;
+		list-style: none;
+	}
+
+	li {
+		margin-right: 20px;
+	}
+</style>
+
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+
+</script>
+
+
+<div class="centeredDiv">
+	<h1>yo, I'm Morgan.</h1>
+	<p>I'm a Computer Networking and Cybersecurity student at Champlain College. I'll write about software, hardware, hiking, and whatever else holds my interest here.</p>
+</div>
+
+
+
+<style>
+	.centeredDiv {
+		margin: auto;
+		width: 75%;
+		padding: 50px;
+	}
+</style>
