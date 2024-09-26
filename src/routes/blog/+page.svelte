c537ee9f9b51d093a6d8d214fc1c66fc80ea963e
Morgan Connors <github@morganconnors.com>
2024-09-26 07:29:13 -0400

    Blog post list page is somewhat functional, except the links don't go anywhere. Fixing soon.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,12 +1,26 @@
-<script>
+<script lang="ts">
+	import * as config from '$lib/config'
 
-
+	export let data
 </script>
 
 <div class="centeredDiv">
 
 	<h1>Blog</h1>
 	<p>This is a work in progress, I don't actually have any content to put here yet.</p>
+
+	<div id="blogPosts">
+		<ul class="posts">
+			{#each data.posts as post}
+				<li class="post">
+					<a href={post.slug} class="title">{post.title}
+					<p class="date">{post.date}</p>
+					<p class="description">{post.description}</p></a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
 
 </div>
 
@@ -15,6 +29,45 @@
 	.centeredDiv {
 		margin: auto;
 		width: 75%;
+	}
+
+	#blogPosts {
+		border-style: solid;
+		border: 3px 0;
+		border-color: black;
+	}
+
+	.posts {
+		border-style: solid;
+		border: 3px 0;
+		border-color: red;
+
+		display: flex;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	.post {
+		border-style: solid;
+		border: 3px 0;
+		border-color: yellow;
+
+		margin: 0;
+		padding: 0;
+		margin-right: 10px;
+	}
+
+	.post a {
+		display: block;
+		height: 100%;
+		width: 100%;
+		color: purple;
+		text-decoration: none;
+	}
+	
+	.post p {
+
 	}
 
 </style>
