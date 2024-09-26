c434ea2228f04a8d608bb6b54fc52c45b4607c6d
Morgan Connors <github@morganconnors.com>
2024-09-25 20:11:37 -0400

    Added content to the resources page.

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -3,9 +3,39 @@
 
 </script>
 
+	<a href=""></a><br>
+
 <div class="centeredDiv">
 
-<h1>Malware</h1>
+	<h1>Resources</h1>
+
+	<p>This is a collection of resources I have come back to more than a few times, so I have begun collecting links here on my site. </p>
+	<p>Just because a link is in one section does not mean it doesn't apply to other sections.</p>
+
+	<h2>Files</h2>
+	<h3>Signatures & Metadata</h3>
+	<a href="https://www.garykessler.net/library/file_sigs.html">Gary Kessler's File Signature Table</a><br>
+	
+	<h2>Hardware Hacking</h2>
+	<h3>Youtube & Blogs</h3>
+	<a href="https://www.youtube.com/@mattbrwn">Matt Brown</a><br>
+
+
+	<h3>Tutorials</h3>
+	<a href="https://www.youtube.com/watch?v=LSQf3iuluYo">Make Me Hack Tutorial Series</a><br>
+
+	<h2>Malware</h2>
+	<h3>Samples</h3>
+	<a href="https://malshare.com/">Malshare</a><br>
+	<a href="https://baazar.abuse.ch/">MalwareBazaar</a><br>
+	<a href="https://vx-underground.org/Samples">vx-underground samples</a><br>
+	<a href="https://virustotal.com/">VirusTotal</a> - Paid offers samples.<br>
+	<h3>Sandboxes</h3>
+	<a href="https://any.run"></a>Any.run<br>
+
+
+
+
 
 </div>
 
@@ -13,5 +43,10 @@
 
 <style>
 
+	.centeredDiv {
+		margin: auto;
+		width: 75%;
+		padding: 50px;
+	}
 
 </style>
