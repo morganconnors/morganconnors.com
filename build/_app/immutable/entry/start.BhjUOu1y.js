b3d1c337b34d0b2141d451af2bdd4e8317bfa957
Morgan Connors <github@morganconnors.com>
2024-10-11 16:45:42 -0400

    Update to resources list + rebuild.

--- build/_app/immutable/entry/start.BhjUOu1y.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DawMq5iv.js";export{t as start};
--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,47 +1,55 @@
 <script>
-	import { title } from '$lib/config.ts'
+    import { title } from "$lib/config.ts";
 </script>
 
 <svelte:head>
-	<title>Resources {title}</title>
+    <title>Resources {title}</title>
 </svelte:head>
 
 <div class="centeredDiv">
+    <h1>Resources</h1>
 
-	<h1>Resources</h1>
+    <p>
+        This is a collection of resources I have come back to more than a few
+        times, so I have begun collecting links here on my site.
+    </p>
+    <p>
+        Just because a link is in one section does not mean it doesn't apply to
+        other sections.
+    </p>
 
-	<p>This is a collection of resources I have come back to more than a few times, so I have begun collecting links here on my site. </p>
-	<p>Just because a link is in one section does not mean it doesn't apply to other sections.</p>
+    <h2>Files</h2>
+    <h3>Signatures & Metadata</h3>
+    <a href="https://www.garykessler.net/library/file_sigs.html"
+        >Gary Kessler's File Signature Table</a
+    ><br />
 
-	<h2>Files</h2>
-	<h3>Signatures & Metadata</h3>
-	<a href="https://www.garykessler.net/library/file_sigs.html">Gary Kessler's File Signature Table</a><br>
-	
-	<h2>Hardware Hacking</h2>
-	<h3>Youtube & Blogs</h3>
-	<a href="https://www.youtube.com/@mattbrwn">Matt Brown</a><br>
+    <h2>Hardware Hacking</h2>
+    <h3>Youtube & Blogs</h3>
+    <a href="https://www.youtube.com/@mattbrwn">Matt Brown</a><br />
 
+    <h3>Tutorials</h3>
+    <a href="https://www.youtube.com/watch?v=LSQf3iuluYo"
+        >Make Me Hack Tutorial Series</a
+    ><br />
 
-	<h3>Tutorials</h3>
-	<a href="https://www.youtube.com/watch?v=LSQf3iuluYo">Make Me Hack Tutorial Series</a><br>
+    <h2>Malware</h2>
+    <h3>Samples</h3>
+    <a href="https://malshare.com/">Malshare</a><br />
+    <a href="https://baazar.abuse.ch/">MalwareBazaar</a><br />
+    <a href="https://vx-underground.org/Samples">vx-underground samples</a><br
+    />
+    <a href="https://virustotal.com/">VirusTotal</a> - Paid offers samples.<br
+    />
+    <h3>Sandboxes</h3>
+    <a href="https://any.run">Any.run</a><br />
 
-	<h2>Malware</h2>
-	<h3>Samples</h3>
-	<a href="https://malshare.com/">Malshare</a><br>
-	<a href="https://baazar.abuse.ch/">MalwareBazaar</a><br>
-	<a href="https://vx-underground.org/Samples">vx-underground samples</a><br>
-	<a href="https://virustotal.com/">VirusTotal</a> - Paid offers samples.<br>
-	<h3>Sandboxes</h3>
-	<a href="https://any.run"></a>Any.run<br>
-
-
-
-
-
+    <h2>Programming</h2>
+    <h3>Version Control</h3>
+    <a href="https://www.nobledesktop.com/learn/git/git-branches"
+        >Git command cheatsheet</a
+    >
 </div>
 
-
-
 <style>
-
 </style>
